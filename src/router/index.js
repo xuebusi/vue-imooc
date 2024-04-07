58cdd3130c3ca2a3f14ac7438b99cb1e87864e0b
xuebusi <xuebusi@qq.com>
2024-04-07 11:41:17 +0800

    修改文件名称

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import NotFound from '@/pages/404.vue'
-import LoginVue from '@/pages/Login.vue'
-import RegisterVue from '@/pages/Register.vue'
-import HomeVue from '@/pages/Home.vue'
-import NewsVue from '@/pages/News.vue'
-import UsersVue from '@/pages/Users.vue'
-import DetailVue from '@/pages/Detail.vue'
+import Login from '@/pages/login.vue'
+import Register from '@/pages/register.vue'
+import Home from '@/pages/home.vue'
+import News from '@/pages/news.vue'
+import Users from '@/pages/users.vue'
+import Detail from '@/pages/detail.vue'
 
 const routes = [
     { path: '/', redirect: '/home' },
-    { path: '/home', component: HomeVue },
-    { path: '/login', component: LoginVue },
-    { path: '/register', component: RegisterVue },
+    { path: '/home', component: Home },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
     {
         path: '/news',
-        component: NewsVue,
+        component: News,
         children: [{
             name: 'detail',
             path: 'detail/:title/:content?',
-            component: DetailVue,
+            component: Detail,
             props: true,
         }]
     },
-    { path: '/users', component: UsersVue },
+    { path: '/users', component: Users },
     // 404页面
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
